0b78bd298ce3e27da0ddcb5a6bb8a18f1f6c5bf5
Karolis Gudas <37053343+zedanas@users.noreply.github.com>
2019-01-30 08:58:25 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,32 +5,32 @@
 # K. G. 29.01.2019                                                                #
 ###################################################################################
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY* -qvy \
-yum install epel-release yum-utils && yum groups install 'Development Tools' 'Compatibility Libraries' -qvy \
-yum install hmaccalc zlib-devel binutils-devel elfutils-libelf-devel ncurses-devel bc wget -qvy \
-yum install tuned-* htop ImageMagick7 -qvy \
+yum install epel-release yum-utils && yum groups install 'Development Tools' 'Compatibility Libraries' -q -v -y \
+yum install hmaccalc zlib-devel binutils-devel elfutils-libelf-devel ncurses-devel bc wget -q -v -y \
+yum install tuned-* htop ImageMagick7 -q -v -y \
 tuned --profile throughput-performance --daemon \
-yum-config-manager --disable remi-php54 \
-yum-config-manager --disable remi-php56 \
-yum-config-manager --disable remi-php70 \
-yum-config-manager --disable remi-php71 \
-yum-config-manager --disable remi-php72 \
-yum-config-manager --enable remi-php73 \
+sudo yum-config-manager --disable remi-php54 \
+sudo yum-config-manager --disable remi-php56 \
+sudo yum-config-manager --disable remi-php70 \
+sudo yum-config-manager --disable remi-php71 \
+sudo yum-config-manager --disable remi-php72 \
+sudo yum-config-manager --enable remi-php73 \
 yum install php php-cli vim php-fpm php-mysqlnd git gitlib php-opcache php-pdo xz lz4 p7zip lzma \
 php-gd php-zip php-devel php-gd php-mcrypt php-mbstring php-xml php-pear php-bcmath php-json php-ldap \
 php-odbc php-zstd php-zstd-devel php-scldevel php-process autoconf automake openssl-devel expat-devel \
-cmake expat-devel libtool composer libnghttp2-devel pcre-devel sudo wget perl pcre-devel libxml2-devel -qvy \
-yum install https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm -qvy \
-yum install mysql-server -qvy \
-yum clean all -v && yum update -qvy  \ 
+cmake expat-devel libtool composer libnghttp2-devel pcre-devel sudo wget perl pcre-devel libxml2-devel -q -v -y \
+yum install https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm  -q -v -y \
+yum install mysql-server  -q -v -y \
+yum clean all -v && yum update  -q -v -y \ 
 mkdir /var/www && mkdir /var/www/html && cd /var/www/html \
 git clone git@github.com:phpmyadmin/phpmyadmind.git \
 cd phpmyadmin && composer update \
 touch /var/www/html/index.php \
 echo "<?php phpinfo(); ?>" >> /var/www/html/index.php \
-yum update -qvy \
+yum update all -q -v -y \
 systemctl start mysqld.service \ 
 systemctl enable mysqld.service \
 php -v && mysql -V \
 -- sudo grep 'temporary password' /var/log/mysqld.log \
 -- mysql_secure_installation \
-ls -all
+ls -ali
