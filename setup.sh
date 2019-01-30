25423ac3ac25bfb39194fd29c47b93041a1f302a
Karolis Gudas <37053343+zedanas@users.noreply.github.com>
2019-01-30 08:48:22 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@
 ###################################################################################
 #!/bin/bash
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY* \
-yum -y -quiet --skip-broken install epel-release yum-utils && yum groups install 'Development Tools' 'Compatibility Libraries' \
-yum -y -quiet --skip-broken install hmaccalc zlib-devel binutils-devel elfutils-libelf-devel ncurses-devel bc wget \
-yum -y -quiet --skip-broken install tuned-* htop ImageMagick7 \
-tuned --profile --skip-broken throughput-performance --daemon \
+yum -y --quiet install epel-release yum-utils && yum groups install 'Development Tools' 'Compatibility Libraries' \
+yum -y --quiet install hmaccalc zlib-devel binutils-devel elfutils-libelf-devel ncurses-devel bc wget \
+yum -y --quiet install tuned-* htop ImageMagick7 \
+tuned --profile throughput-performance --daemon \
 yum-config-manager --disable remi-php54 \
 yum-config-manager --disable remi-php56 \
 yum-config-manager --disable remi-php70 \
@@ -19,9 +19,9 @@ yum -y -quiet --skip-broken install php php-cli vim php-fpm php-mysqlnd git gitl
 php-gd php-zip php-devel php-gd php-mcrypt php-mbstring php-xml php-pear php-bcmath php-json php-ldap \
 php-odbc php-zstd php-zstd-devel php-scldevel php-process autoconf automake openssl-devel expat-devel \
 cmake expat-devel libtool composer libnghttp2-devel pcre-devel sudo wget perl pcre-devel libxml2-devel \
-yum -y -quiet --skip-broken install https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm \
-yum -y -quiet --skip-broken  install mysql-server \
-yum -y -quiet --skip-broken  clean all -v && yum update  \ 
+yum -y --quiet install https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm \
+yum -y --quiet  install mysql-server \
+yum -y --quiet  clean all -v && yum update  \ 
 mkdir /var/www && mkdir /var/www/html && cd /var/www/html \
 git clone git@github.com:phpmyadmin/phpmyadmind.git \
 cd phpmyadmin && composer update \
